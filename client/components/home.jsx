528d609867b8790d32803a2d3dab99e4a8f7f632
Vitaly K <kuvini@gmail.com>
2021-10-23 17:25:36 +0500

    GitBrowser

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -1,18 +1,45 @@
 import React, { useState } from 'react'
-import Head from './head'
-// import wave from '../assets/images/wave.jpg'
+import { Link } from 'react-router-dom'
 
-const Home = () => {
-  const [counter, setCounterNew] = useState(0)
+import Head from './head'
+
+import { history } from '../redux'
+
+const Home = ({ setUser }) => {
+  const [value, setValue] = useState('')
+
+  // const onChange = (e) => {
+  //   setValue(e.target.value)
+  // }
+
+  const onClick = () => {
+    setUser(value)
+    history.push(`/${value}`)
+  }
 
   return (
-    <div>
-      <Head title="Hello" />
-      <img alt="wave" src="images/wave.jpg" />
-      <button type="button" onClick={() => setCounterNew(counter + 1)}>
-        updateCounter
-      </button>
-      <div> Hello World Dashboard {counter} </div>
+    <div className="flex justify-center items-center">
+      <Head title="Welcome" />
+      <div className="flex flex-col bg-gray-100 rounded border m-2 p-4 space-y-2">
+        <input
+          className="rounded p-2"
+          type="text"
+          id="input-field"
+          onChange={(e) => setValue(e.target.value)}
+          value={value}
+        />
+        <button
+          className="border bg-gray-300 rounded p-2"
+          type="button"
+          id="search-button"
+          onClick={onClick}
+        >
+          Enter
+        </button>
+        <Link className="border bg-gray-300 rounded p-2" id="search-link" to={`/${value}`}>
+          Enter with Link
+        </Link>
+      </div>
     </div>
   )
 }
---
+++ client/components/readme.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+// import { useParams } from 'react-router-dom'
+import ReactMarkdown from 'react-markdown'
+
+import Head from './head'
+import Header from './header'
+
+const Readme = ({ user, repo }) => {
+  // const { userName, repositoryName } = useParams()
+  const [text, setText] = useState('')
+  const url = `https://raw.githubusercontent.com/${user}/${repo}/master/README.md`
+  useEffect(() => {
+    fetch(url)
+      .then((r) => r.text())
+      .then((str) => {
+        setText(str)
+      })
+      .catch((err) => console.log(err))
+  }, [url])
+  return (
+    <>
+      <Head title="Readme" />
+      <Header {...{ user, repo }} />
+      <div id="description" className="m-2">
+        <ReactMarkdown>
+          {text}
+        </ReactMarkdown>
+      </div>
+    </>
+  )
+}
+
+Readme.propTypes = {}
+
+export default React.memo(Readme)
